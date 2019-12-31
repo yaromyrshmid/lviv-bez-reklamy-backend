4899acb8c597c3cd4e5f94ef1deeeb6740d7840b
yaromyrshmid <yaromyr.shmid@gmail.com>
2019-12-31 10:20:30 +0200

    Moved keys to production and dev

---
+++ config/keys_prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secret: process.env.MONGO_SECRET,
+  googleAuth: {
+    clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_AUTH_CALLBACK_URL
+  },
+  googleMapAPI: process.env.GOOGLE_MAP_API
+};
